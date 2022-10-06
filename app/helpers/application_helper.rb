7eff13d139c4701090bbb45ed9336a25fc628bad
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-06 19:07:49 +0100

    avatar created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
-	def avatar_url(user)
+
+  def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		if user.image
-			user.image
-		else
-			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-		end
-	end
+   "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+  end
 end
