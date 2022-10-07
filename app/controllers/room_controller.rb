01de5d2e711c2a97caec3d24e355703604ebd1b8
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-07 10:20:55 +0100

    Room controller done

---
+++ app/controllers/room_controller.rb
@@ -0,0 +1,57 @@
+class RoomController < ApplicationController
+  before_action :set_room, except: %i[index new create]
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def new
+    @room = current_user.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      redirect_to listing_room_path(@room), notice: "Saved...."
+    else
+      render :new, notice: "Something Went wrong"
+    end
+  end
+
+  def listing
+  end
+
+  def pricing
+  end
+
+  def description
+  end
+
+  def photo_upload
+  end
+
+  def amenities
+  end
+
+  def location
+  end
+
+  def update
+    if @room.update(room_params)
+      flash[:notice] = "Saved"
+    else
+      flash[:notice] = "Something Went Wrong"
+      redirect_back(fallback_location: request.referer)
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room, :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating, :is_internet, :price, :active )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'room/index'
+  get 'room/new'
+  get 'room/create'
+  get 'room/listing'
+  get 'room/pricing'
+  get 'room/description'
+  get 'room/photo_upload'
+  get 'room/amenities'
+  get 'room/location'
+  get 'room/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
