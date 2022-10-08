b4a1490962ecee55decfb02ad7d5f9af6ed40668
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-08 14:04:41 +0100

    add bookings

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,8 @@ class Room < ApplicationRecord
 
   validates :home_type, presence: true
   validates :room_type, presence: true
-  validates :accommodate, presence: true
+  validates :acommodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
-  
+
 end
