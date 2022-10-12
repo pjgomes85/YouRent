aa0f0fef90c77ddcf6deaae8283e19160d4cb878
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-12 15:23:49 +0100

    debugging photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_132212) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_142216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_132212) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_photos_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -88,5 +95,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_132212) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "photos", "rooms"
   add_foreign_key "rooms", "users"
 end
