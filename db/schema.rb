3890bba132f1b61af48ed32d3cdd98f104f3acf8
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-12 15:04:17 +0100

    adding photos controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_090346) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_132212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_090346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "photo"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
