ef02b499d708bfe4e006ec18601526cfe715c08a
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-13 17:00:31 +0100

    styling

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,12 @@ class Room < ApplicationRecord
   validates :acommodate, presence: true
   validates :bed_room, presence: true
   validates :bath_room, presence: true
+
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "logo.png"
+    end
+  end
 end
