f28f6d0a3bf0d2a1a87ba673ca8968d913633af6
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-14 12:19:08 +0100

    add migration cordnates Rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_153640) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_111821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_153640) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
