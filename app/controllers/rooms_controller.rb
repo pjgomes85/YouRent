9d0a358315f17291fde58b221c8713523342e7cb
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-14 19:20:54 +0100

    adding info window with picture

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -49,7 +49,7 @@ class RoomsController < ApplicationController
       {
         lat: room.latitude,
         lng: room.longitude,
-        # info_window: render_to_string(partial: "info_window", locals: {room: room}),
+        info_window: @room.cover_photo(:thumb)
         # image_url: helpers.asset_url("logo.png")
       }
     end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -15,19 +15,29 @@ export default class extends Controller {
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v8',
       center: [-0.5800364, 44.841225],
-      zoom: 13
+      zoom: 15
     });
 
     this.#addMarkersToMap()
-    // this.#fitMapToMarkers()
+    this.#fitMapToMarkers()
     // this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
 
 
   }
+
+    #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 8, duration: 150 })
+  }
+
+
     #addMarkersToMap() {
       this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(this.map);
 
 
