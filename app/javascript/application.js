bc24b4914d9422729fc60ef4b38ab1ec812dc5e4
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-16 15:37:52 +0100

    removed all the Jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Entry point for the build script in your package.json
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui/widgets/datepicker
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
