c9946d4a2457454c07d65241b9cd0bd3f42b80de
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-16 11:42:27 +0100

    booking system working

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,12 +5,20 @@ class ReservationsController < ApplicationController
 
     start_date = Date.parse(reservation_params[:start_date])
     end_date = Date.parse(reservation_params[:end_date])
-    days = (end_date - start_date).to_i + 1 
+    days = (end_date - start_date).to_i + 1
+
+    @reservation = current_user.reservations.build(reservation_params)
+    @reservation.room = room
+    @reservation.price = room.price
+    @reservation.total = room.price * days
+
+    flash[:notice] = "Booked Successfully"
+    redirect_to room
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).premit(:start_date, :end_date)
+    params.require(:reservation).permit(:start_date, :end_date)
   end
 end
