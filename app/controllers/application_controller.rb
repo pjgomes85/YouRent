4cb31f5dbdb85ed388b96fa7c4338e2dc66d4fc1
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-18 01:37:26 +0100

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_paramters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname phone_number description])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname phone_number description image])
   end
 end
