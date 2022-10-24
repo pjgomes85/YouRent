48b4aa9d8ad270e1261757df275c753feae5bf6c
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-24 09:03:11 +0100

    added profile picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def avatar_url(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-   "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+    	# 	gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  #  "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
   end
 end
