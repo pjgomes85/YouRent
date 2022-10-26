684d97474d8973a34e4daabbfd55ac3e3880bd19
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-26 13:52:52 +0100

    more fixing

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,13 +13,13 @@ class Room < ApplicationRecord
   validates :bed_room, presence: true
   validates :bath_room, presence: true
 
-  def cover_photo(size)
-    if self.photos.length > 0
-      self.photos[0].image.url(size)
-    else
-      "No-Image.jpeg"
-    end
-  end
+  # def cover_photo
+  #   if self.photos.length > 0
+  #     self.photos[0].image.url(size)
+  #   else
+  #     "No-Image.jpeg"
+  #   end
+  # end
 
   def average_rating
     reviews.count == 0 ? 0 : reviews.average(:star).round(2)
