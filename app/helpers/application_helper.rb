1a88f11b1a72d17135bc6b6d4133d4cced3c1ce6
Paulo Gomes <pjgomes85@hotmail.com>
2022-10-27 20:34:52 +0100

    profile picture working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,12 @@
-require "open-uri"
-
 module ApplicationHelper
-
   def avatar_url1(user)
     "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
     	# 	gravatar_id = Digest::MD5::hexdigest(user.email).downcase
   #  "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
   end
 
-def avatar_url(user)
-  if user.avatar.attached?
+  def avatar_url(user)
+    user.avatar.attached?
     user.avatar.key
-  else
-    URI.open("https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png")
   end
-end
-
-
 end
