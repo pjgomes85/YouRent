98653b3b083c12d451fa0f00f8443661dbb51e7b
Paulo Gomes <pjgomes85@hotmail.com>
2022-12-27 16:35:53 +0000

    geocodin autocomplete working" "

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -4,7 +4,6 @@ import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 // Connects to data-controller="address-autocomplete"
 export default class extends Controller {
   static values = { apiKey: String }
-
   static targets = ["address"]
 
   connect() {
