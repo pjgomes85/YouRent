898e2e65b69645b341374c189391c53aad204c6d
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-02 16:30:19 +0000

    star review working

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  
+
+  validates :star, presence: true, numericality:
+            { greater_than_or_equal_to: 1, less_than_or_equal_to: 5,
+              only_integer: true }
+
+  # def update_reviewable_rating
+  #   reviewable.update! average_rating: reviewable.reviews.average(:star)
+  #   # avg + (rating - avg) / count
+  # end
 end
