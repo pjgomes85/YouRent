3e7c42487d703e5fbc945c0a594813dd32751e52
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-05 21:18:30 +0000

    fixing booking form bug

---
+++ app/javascript/bookingForm.js
@@ -0,0 +1,13 @@
+//booking-form sticky scroll
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
