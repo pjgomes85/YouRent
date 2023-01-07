7c66d8db5ad904b0f581615c64a3450f081f2891
Paulo Gomes <pjgomes85@hotmail.com>
2023-01-07 18:59:12 +0000

    bootsrap 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  add_flash_types :danger, :info, :warning, :success, :messages
+ 
 
   before_action :configure_permitted_paramters, if: :devise_controller?
 
